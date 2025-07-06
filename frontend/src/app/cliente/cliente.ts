be0a6521dbe9437529990f781c139923ee4872dc
Rodrigo Est Pal <restefanerop@unsa.edu.pe>
2025-07-06 16:51:39 -0500

    Añadir funcionalidad de búsqueda de restaurantes y mejorar la vista de cliente

--- frontend/src/app/cliente/cliente.ts
+++ frontend/src/app/cliente/cliente.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cliente',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './cliente.html',
   styleUrl: './cliente.css'
 })
 export class Cliente implements OnInit {
   restaurantes: any[] = [];
+  busqueda: string = '';
 
   constructor(private http: HttpClient) {}
 
@@ -18,6 +20,12 @@ export class Cliente implements OnInit {
       .subscribe(data => {
         this.restaurantes = data;
       });
+  }
+
+  get restaurantesFiltrados() {
+    return this.restaurantes.filter(r =>
+      r.nombre.toLowerCase().includes(this.busqueda.toLowerCase())
+    );
   }
 
   verPedidos() {
