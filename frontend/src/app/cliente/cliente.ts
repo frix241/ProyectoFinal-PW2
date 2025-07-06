b0cbad6f125e0a2cd5641cdec8dad01781bf07e7
Rodrigo Est Pal <restefanerop@unsa.edu.pe>
2025-07-06 16:28:45 -0500

    Implementación de la vista de cliente con búsqueda de restaurantes y funcionalidad de pedidos

--- frontend/src/app/cliente/cliente.ts
+++ frontend/src/app/cliente/cliente.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cliente',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cliente.html',
   styleUrl: './cliente.css'
 })
-export class Cliente {
+export class Cliente implements OnInit {
+  restaurantes: any[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<any[]>('http://127.0.0.1:8000/api/restaurantes/')
+      .subscribe(data => {
+        this.restaurantes = data;
+      });
+  }
+
+  verPedidos() {
+    // TODO: Implementar vista de pedidos
+    alert('Funcionalidad de pedidos próximamente');
+  }
+
+  verRestaurante(id: number) {
+    // TODO: Implementar vista de restaurante individual
+    alert(`Ver restaurante ID: ${id}`);
+  }
 }
