075d24883ff77f9a88500c928d2c7068f2edfadc
frix241 <ariverosv@unsa.edu.pe>
2025-07-06 13:21:27 -0500

    Configurando la ruta para el registro de nuevos usuarios

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { Login } from './login/login';
 import { Cliente } from './cliente/cliente';
 import { RestaurantePanel } from './restaurante-panel/restaurante-panel';
-
+import { Register } from './register/register';
 export const routes: Routes = [
   { path: 'login', component: Login },
+  { path: 'register', component: Register },
   { path: 'cliente', component: Cliente },
   { path: 'restaurante', component: RestaurantePanel },
   { path: '', redirectTo: '/login', pathMatch: 'full' }
