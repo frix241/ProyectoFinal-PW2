a648dcf15c982042c30c4b674561b09b042c3e4b
Rodrigo Est Pal <restefanerop@unsa.edu.pe>
2025-07-05 23:46:07 -0500

    Implementar vistas para Restaurant y Menu con permisos de acceso

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Restaurant, Menu
+from .serializers import RestaurantSerializer, MenuSerializer
 
-# Create your views here.
+class RestaurantViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+class MenuViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = MenuSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        restaurante_id = self.request.query_params.get('restaurante_id')
+        if restaurante_id:
+            return Menu.objects.filter(restaurante_id=restaurante_id).order_by('-fecha')
+        return Menu.objects.all().order_by('-fecha')
