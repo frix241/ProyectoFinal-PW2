1541f7cd729424fbed3523151df6f70036347a1e
J-Gabriel-Q-M <josquispem@unsa.edu.pe>
2025-07-06 22:20:39 -0500

    importes correctamente RestaurantePanelComponent

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
 import { Login } from './login/login';
 import { Cliente } from './cliente/cliente';
-import { RestaurantePanel } from './restaurante-panel/restaurante-panel';
+import { RestaurantePanelComponent } from './restaurante-panel/restaurante-panel';
 import { Register } from './register/register';
 export const routes: Routes = [
   { path: 'login', component: Login },
   { path: 'register', component: Register },
   { path: 'cliente', component: Cliente },
-  { path: 'restaurante', component: RestaurantePanel },
+  { path: 'restaurante', component: RestaurantePanelComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
