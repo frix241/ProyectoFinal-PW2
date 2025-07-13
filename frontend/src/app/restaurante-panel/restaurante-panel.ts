03a3f7e727c68e26d0c5159522c7cd7eaa0e73f8
brayan <bauccacusic@unsa.edu.pe>
2025-07-13 16:30:37 -0500

    definiendo la clase restaurantePanel e importando el modulo forms

--- frontend/src/app/restaurante-panel/restaurante-panel.ts
+++ frontend/src/app/restaurante-panel/restaurante-panel.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-restaurante-panel',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './restaurante-panel.html',
   styleUrl: './restaurante-panel.css'
 })
 export class RestaurantePanel {
-
+  nombreRestaurante: string = '';
 }
