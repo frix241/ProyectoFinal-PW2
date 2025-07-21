d9f6cb087a9933dfb773e7dd364bbbc0196db160
brayan <bauccacusic@unsa.edu.pe>
2025-07-21 09:09:23 -0500

    modificando los serializer para que acepten imagenes y obteniendo las urls

--- restaurants/serializers.py
+++ restaurants/serializers.py
@@ -7,14 +7,35 @@ class RestaurantSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class EntradaSerializer(serializers.ModelSerializer):
+    imagen_url = serializers.SerializerMethodField()
+
+    def get_imagen_url(self, obj):
+        if obj.imagen:
+            return self.context['request'].build_absolute_uri(obj.imagen.url)
+        return None
+
     class Meta:
         model = Entrada
         fields = '__all__'
+        extra_kwargs = {
+            'imagen': {'required': False, 'write_only': True}
+        }
 
 class SegundoSerializer(serializers.ModelSerializer):
+    imagen_url = serializers.SerializerMethodField()
+
+    def get_imagen_url(self, obj):
+        if obj.imagen:
+            return self.context['request'].build_absolute_uri(obj.imagen.url)
+        return None
+
     class Meta:
         model = Segundo
         fields = '__all__'
+        extra_kwargs = {
+            'imagen': {'required': False, 'write_only': True}
+        }
+
 
 class MenuSerializer(serializers.ModelSerializer):
     entradas = EntradaSerializer(many=True, read_only=True)
