2a88d319bd8e96622ec257710fe041a0426542dd
Rodrigo Est Pal <retefanerop@unsa.edu.pe>
2025-07-26 18:31:21 -0500

    pagina login + css creado

--- frontend/src/app/auth/login/login.ts
+++ frontend/src/app/auth/login/login.ts
@@ -8,7 +8,8 @@ import { Router } from '@angular/router';
   selector: 'app-login',
   standalone: true,
   imports: [FormsModule, NgIf],
-  templateUrl: './login.html'
+  templateUrl: './login.html',
+  styleUrls: ['./login.css']
 })
 export class LoginComponent {
   username = '';
